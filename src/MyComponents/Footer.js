7f09cb9edef5e387408214aac39f6685a49b13be
thisispramod <itspramod19@gmail.com>
2024-12-30 00:18:20 +0530

    used here style css as object when you need to add many style css its better to use object style

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
+  let footerStyle = {
+    position: "absolute",
+    top:"100vh",
+    width:"100%",
+    backgroundColor:"rgba(5, 0, 0, 0.05)"
+  }
   return (  
-      <div className="text-center p-3" style={{backgroundColor:' rgba(0, 0, 0, 0.05)'}}>
+      <div className="text-center p-3" style={footerStyle}>
         © 2020 Copyright:
         <a style={{textDecoration:'none', fontWeight:'bold'}} className="text-body" href="javasript:void(0)">Todo App (Todospecial YT)</a> 
       </div>  
